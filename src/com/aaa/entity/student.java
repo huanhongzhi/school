3865f88d6b0609df3a361851f9592f00328820cc
huanhongzhi <975512883@qq.com>
2017-03-23 15:22:19 +0800

    学习

---
+++ src/com/aaa/entity/student.java
@@ -0,0 +1,18 @@
+package com.aaa.entity;
+
+public class student {
+	private Integer stuid;
+	public Integer getStuid() {
+		return stuid;
+	}
+	public void setStuid(Integer stuid) {
+		this.stuid = stuid;
+	}
+	public String getStuname() {
+		return stuname;
+	}
+	public void setStuname(String stuname) {
+		this.stuname = stuname;
+	}
+	private String stuname;
+}
