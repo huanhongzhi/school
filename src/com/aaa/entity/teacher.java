6ccdf36f5c916335b66817223030087ed08e8758
huanhongzhi <975512883@qq.com>
2017-03-23 15:37:56 +0800

    重新写的

---
+++ src/com/aaa/entity/teacher.java
@@ -0,0 +1,20 @@
+package com.aaa.entity;
+
+public class teacher {
+	private Integer teaid;
+	public Integer getTeaid() {
+		return teaid;
+	}
+	public void setTeaid(Integer teaid) {
+		this.teaid = teaid;
+	}
+	public String getTeaname() {
+		return teaname;
+	}
+	public void setTeaname(String teaname) {
+		this.teaname = teaname;
+	}
+	private String teaname;
+	
+
+}
